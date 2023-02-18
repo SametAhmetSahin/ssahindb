0f2e2eea162729844733950054f5884ebd735c5f
Samet Ahmet Şahin <samet.sahin@bgasecurity.com>
2023-02-18 20:00:45 +0300

    Initial implementation with files being written as default. There are set, get and del commands.

---
+++ src/cli.rs
@@ -0,0 +1,49 @@
+use std::io;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+
+    let address = "0.0.0.0:50394";
+
+    loop {
+
+    let mut lock = io::stdout().lock();
+
+    write!(lock, "{}> ", address).expect("Couldn't write to terminal");
+    //print!("{}> ", address);
+
+    io::stdout().flush().expect("Couldn't flush stdout");
+
+    let mut input = String::from("");
+    io::stdin().read_line(&mut input).expect("Couldn't read input");
+
+    //println!("Sending message:\n{}", &input);
+
+        if input != "\n" {
+            while !match TcpStream::connect(address) {
+                Ok(mut stream) => {
+                    //println!("input as bytes: {:?}", input.as_bytes());
+                    stream.write((&input[0..input.len()-1]).as_bytes())?;
+                    //stream.flush(); 
+                    let mut resbuffer = vec![0 as u8; 512];
+                    stream.read(&mut resbuffer);
+                    
+                    //println!("resbuffer: {:?}", resbuffer);
+
+                    println!("{:?}", String::from_utf8_lossy(&resbuffer).replace("\0", ""));
+
+                    stream.shutdown(std::net::Shutdown::Both).expect("Couldn't shut down the connection");
+                    true
+                },
+                Err(_) => {
+                    println!("An error occured while trying to connect to {}. Trying again in 3 seconds.", address);
+                    
+                    std::thread::sleep(std::time::Duration::from_secs(3));
+                    false
+                }
+            } {}
+        }
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+use std::fs::File;
+use std::fs;
+use std::io::Write;
+use std::process::exit;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+
+fn main() {
+    println!("SSahinDB starting...");
+
+    //set_key("test1".to_string(), "test1".to_string());
+
+    //println!("{}", get_key("test1".to_string()));
+    start_listener().expect("Couldn't start server");
+}
+
+fn start_listener() -> std::io::Result<()> {
+    
+    let listener = TcpListener::bind("0.0.0.0:50394")?;
+    
+    for stream in listener.incoming() {
+        handle_client(stream?)
+    }
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let bufsize = 8192;
+    let mut buf = vec![0 as u8; bufsize+1];
+    match stream.read(&mut buf) {
+        Ok(_) => {
+            let strbuf = String::from_utf8_lossy(&buf);
+            //println!("strbuf {}", strbuf);
+            //println!("buf {:?}", &buf);
+            let replaced = strbuf.replace("\0", "");
+            let split: Vec<&str> = replaced.split(" ").collect();
+            let res = handle_command(split);
+            //println!("res is: {}", res);
+            stream.write(res.as_bytes());
+            //println!("splitvec {:?}", split);
+            //stream.shutdown(std::net::Shutdown::Both).expect("Couldn't shut down the connection");
+            buf = vec![0 as u8; bufsize+1];
+            
+        },
+        Err(_) => {
+            println!("An error occured!");
+            
+            exit(1)
+        }
+    };
+
+    
+    
+}
+
+fn handle_command(args: Vec<&str>) -> String {
+    println!("it is {}", args[0]);
+    match args[0] {
+         "get" => {
+            return get_key(args[1].to_string()).to_string();
+         },
+         "set" => {
+            if args.len() != 3 {
+                return String::from("Invalid number of arguments!")
+            }
+
+            return set_key(args[1].to_string(), args[2].to_string());
+         },
+         "del" => {
+            delete_key(args[1].to_string())
+         },
+         _ => {
+
+            println!("invalid command!");
+            return format!("Invalid command {}, with args {:?}!", args[0], &args[1..args.len()]);
+         },
+
+    }
+}
+
+fn write_file(path: String, content: String) -> std::io::Result<()> {
+
+    let mut file = File::create(path)?;
+    write!(file, "{}", content)
+}
+
+
+fn set_key(key: String, value: String) -> String {
+    match write_file(String::from("db/") + &key, value) {
+        Ok(_) => {
+            //format!("{}", key)
+            String::from("OK")
+        },
+        Err(e) => {format!("{}", e)}
+    }
+}
+
+fn file_exists(path: String) -> bool {
+
+    match fs::metadata(&path) {
+        Ok(_) => { true },
+        Err(_) => { false }
+    }
+
+}
+
+fn delete_file(path: String) -> std::io::Result<()> {
+    match fs::remove_file(path) {
+        Ok(_) => { Ok(()) },
+        Err(e) => { Err(e) }
+    }
+}
+
+fn delete_key(key: String) -> String {
+    if file_exists(String::from("db/") + &key) {
+
+    match delete_file(String::from("db/") + &key) {
+        Ok(_) => {
+            //format!("{}", key)
+            1.to_string()
+        },
+        Err(e) => {format!("{}", e)}
+    }
+
+    }
+    else {
+        
+        0.to_string()
+    }
+}
+
+fn read_file(path: String) -> Result<String, std::io::Error> {
+
+    println!("read file path is: {}", path);
+    if file_exists(path.clone()) {
+        match fs::read_to_string(path) {
+            Ok(contents) => { println!("Contents: {}", contents); Ok(contents) },
+            Err(e) => {Err(e)}
+        }
+    }
+    else {
+        Ok("nil".to_string())
+    }
+    
+}
+
+
+fn get_key(key: String) -> String {
+    return read_file(String::from("db/") + &key).expect(&("Couldn't get key ".to_string() + &key))
+}
